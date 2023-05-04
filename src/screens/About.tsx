fdb66daa2b974ea9f1bd2133dc1edf57f53a3bea
Felipe Bassi <fkb5105@psu.edu>
2023-05-04 18:35:47 -0400

    About page server connection

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Linking } from 'react-native';
 import { COLORS } from '../constants/light';
-import axios from 'axios';
 
-interface AboutData {
+interface AboutInfo {
   about_id: number;
   image_url: string;
   name: string;
@@ -12,35 +11,40 @@ interface AboutData {
 }
 
 const About = () => {
-  const [aboutData, setAboutData] = useState<AboutData[]>([]);
+  const [aboutData, setAboutData] = useState<AboutInfo[]>([]);
+
+  async function fetchAboutData() {
+    try {
+      const response = await fetch('http://146.186.8.211:8080/about');
+      console.log('Response status:', response.status);
+      const data = await response.json();
+      console.log('Data:', data);
+      setAboutData(data);
+    } catch (error) {
+      console.error('Error fetching about data:', error);
+    }
+  }
+  
 
   useEffect(() => {
-    axios.get('http://localhost:5000/ABOUT')
-      .then(response => {
-        setAboutData(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    fetchAboutData();
   }, []);
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>Meet the Team</Text>
-      {aboutData.map((member: AboutData) => (
-        <View style={styles.card} key={member.about_id}>
-          <Image style={styles.avatar} source={{ uri: member.image_url }} />
+      {aboutData.map((about) => (
+        <View style={styles.card} key={about.about_id}>
+          <Image style={styles.avatar} source={{ uri: about.image_url }} />
           <View style={styles.info}>
-            <Text style={styles.name}>{member.name}</Text>
-            <Text style={styles.position}>{member.position}</Text>
-            <Text style={styles.about}>{member.description}</Text>
+            <Text style={styles.name}>{about.name}</Text>
+            <Text style={styles.position}>{about.position}</Text>
+            <Text style={styles.about}>{about.description}</Text>
           </View>
         </View>
       ))}
       <View style={styles.projectSection}>
-        <Text style={styles.projectText}>
-          See what else VAR Labs is working on
-        </Text>
+        <Text style={styles.projectText}>See what else VAR Labs is working on</Text>
         <TouchableOpacity style={styles.visitButton} onPress={() => Linking.openURL('https://var.psu.edu/projects/')}>
           <Text style={styles.buttonText}>Visit Projects</Text>
         </TouchableOpacity>
@@ -52,7 +56,7 @@ const About = () => {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     paddingVertical: 20,
     paddingHorizontal: 30,
@@ -95,8 +99,7 @@ const styles = StyleSheet.create({
     color: '#777',
     marginBottom: 10,
   },
-  about:
- {
+  about: {
     fontSize: 14,
     lineHeight: 20,
   },
@@ -108,15 +111,18 @@ const styles = StyleSheet.create({
   projectText: {
     fontSize: 24,
     fontWeight: 'bold',
-    margin:10,
+    margin: 10,
     marginBottom: 16,
     color: '#333',
     textAlign: 'center',
   },
+
   visitButton: {
     backgroundColor: COLORS.tertiary,
     paddingVertical: 10,
     paddingHorizontal: 30,
+   
+
     borderRadius: 5,
   },
   buttonText: {
